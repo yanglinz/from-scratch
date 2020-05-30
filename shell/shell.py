ce23ffb32ad5a8d7e9e6b3f24583566a3fbd3e96
Yanglin Zhao <yz1876@gmail.com>
2020-05-30 00:09:31 -0400

    Add the progress on pipeline executor

--- shell/shell.py
+++ shell/shell.py
@@ -3,11 +3,15 @@ import subprocess
 from lark import Lark
 
 parser_grammar = r"""
-start: COMMAND(SPACE COMMAND)*
+start: command_line
 
-COMMAND: /[^\s]+/
+command_line: command | pipeline
+pipeline: command SPACE PIPE SPACE command_line
+command: COMMAND(SPACE COMMAND)*
 
 SPACE: " "+
+PIPE: "|"
+COMMAND: /[^\s|]+/
 """
 
 parser = Lark(parser_grammar)
@@ -24,8 +28,12 @@ class Command:
         subprocess.call(proc_args)
 
 
-class Shell:
-    pass
+class Pipeline:
+    def __init__(self, parsed_tree):
+        pass
+
+    def execute(self):
+        pass
 
 
 def main():
