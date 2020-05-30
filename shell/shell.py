2b1a880a27ae00d80c579c86c78c51509484495d
Yanglin Zhao <yz1876@gmail.com>
2020-05-29 22:27:52 -0400

    Execute any command

--- shell/shell.py
+++ shell/shell.py
@@ -1,12 +1,27 @@
+import subprocess
+
 from lark import Lark
 
 parser_grammar = r"""
-start: PIPE+
+start: COMMAND(SPACE COMMAND)*
 
-PIPE: "|"
+COMMAND: /[^\s]+/
+
+SPACE: " "+
 """
 
 parser = Lark(parser_grammar)
+
+
+class Command:
+    def __init__(self, parsed_tree):
+        args = parsed_tree.children
+        args = [a for a in args if a.type != "SPACE"]
+        self.args = args
+
+    def execute(self):
+        proc_args = [a.value for a in self.args]
+        subprocess.call(proc_args)
 
 
 class Shell:
@@ -15,10 +30,10 @@ class Shell:
 
 def main():
     user_input = input("> ")
-    print(user_input)
+    parsed_input = parser.parse(user_input)
+    cmd = Command(parsed_input)
+    cmd.execute()
 
 
 if __name__ == "__main__":
-    # main()
-    parsed = parser.parse("|||")
-    print(parsed)
+    main()
